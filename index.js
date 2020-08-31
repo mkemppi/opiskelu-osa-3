0c56a9cc2d9dc427cece5d0f37c336eca6119156
Mikko Kemppi <mikko.kemppi@tahko.com>
2020-08-31 17:16:09 +0300

    puhelinluettelon backend step1

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+  {
+    id: 1,
+    name: "Mikko Kemppi",
+    phone: "0447481114"
+  },
+  {
+    id: 2,
+    name: "Teuvo Karjapää",
+    phone: "044432432"
+  },
+  {
+    id: 3,
+    name: "Teija Tepsukka",
+    phone: "05050505050"
+  },
+  {
+    id: 4,
+    name: "Kalja Kuuppanen",
+    phone: "0934090909"
+  }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Puhelinluettelo!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
