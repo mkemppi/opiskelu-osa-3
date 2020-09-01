a24717e8a185d8f2dade8bf0a2d8737b60281d09
Mikko Kemppi <mikko.kemppi@tahko.com>
2020-09-01 08:24:34 +0300

    puhelinluettelon backend step7

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
-
 app.use(express.json()) 
+app.use(morgan('tiny'))
+
+
+/*
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)*/
 
 let persons = [
   {
@@ -97,7 +110,13 @@ app.post('/api/persons', (request, response) => {
 
   response.json(person)
 })
+/*
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
+app.use(unknownEndpoint)
+*/
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
